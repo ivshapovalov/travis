a24d25d62429c02a8f99d16dea2778f51295cc56
stramak <shtramak@gmail.com>
2018-01-17 10:11:58 +0200

    initial commit

---
+++ src/main/java/com/ua/shtramak/travis/Travis.java
@@ -0,0 +1,7 @@
+package com.ua.shtramak.travis;
+
+public class Travis {
+    public String helloTravis(){
+        return "Hello Travis";
+    }
+}
---
+++ src/test/java/com/ua/shtramak/travis/TravisTest.java
@@ -0,0 +1,12 @@
+package com.ua.shtramak.travis;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TravisTest {
+    @Test
+    public void helloInAnyCaseReturnsHelloTravis() {
+        assertEquals("Hello Travis!", new Travis().helloTravis());
+    }
+}
